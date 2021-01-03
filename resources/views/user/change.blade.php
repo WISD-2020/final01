910b0e25c25ed4c0a63df2d11cd947116a5940af
Lin Yu Lun <wuyahayaa01@gmail.com>
2021-01-03 14:29:18 +0800

    會員修該會員資料頁面

--- resources/views/user/change.blade.php
+++ resources/views/user/change.blade.php
@@ -1,9 +1,29 @@
+<form action="/user/{{$user->id}}" method="POST" role="form">
+    @method('PATCH')
+    @csrf
 <H1>修改會員資料</H1>
-會員名稱：{{$user->name}}<br>
-性別：{{$user->sex}}<br>
-生日：{{$user->birthday}}<br>
-電話：{{$user->phone}}<br>
-電子郵件：{{$user->email}}<br>
+
+會員名稱：<br>
+{{$user->name}}<br><br>
+
+性別：<br>
+    <select id="sex" name="sex" class="form-control">
+        <option value="1" {{(!$user->sex)?'selected':''}}>男</option>
+        <option value="0" {{($user->sex)?'selected':''}}>女</option>
+    </select><br><br>
+
+生日：<br>
+<input type="date" name="birthday" value="{{$user->birthday}}"><br><br>
+
+電話：<br>
+<input type="text" name="phone" value="{{$user->phone}}"><br><br>
+
+電子郵件：<br>
+    <input type="text" name="email" value="{{$user->email}}"> <br><br><br><br>
+
+    <input type="button" name="submit" value="修改">
+
+</form>
 
 
 
