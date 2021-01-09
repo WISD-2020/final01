32ee64f027163f1d523309c3086ee08fee2240a4
Lin Yu Lun <wuyahayaa01@gmail.com>
2021-01-09 23:26:37 +0800

    更新：選單調整

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -12,16 +12,16 @@
                     <a class="nav-link" href="{{route('dashboard')}}">首頁</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('user.change')}}">會員修改資料</a>
+                    <a class="nav-link" href="{{route('posts.show')}}">購物車</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('order.history')}}">點餐紀錄</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('posts.show')}}">Sample Post</a>
+                    <a class="nav-link" href="{{route('user.change')}}">會員修改資料</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('posts.contact')}}">Contact</a>
+                    <a class="nav-link" href="{{route('posts.contact')}}">問題回報</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('user.logout')}}">登出</a>
