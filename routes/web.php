1f6ab42bb82e45cdec367837414a0d98fc859244
Lin Yu Lun <wuyahayaa01@gmail.com>
2021-01-10 16:37:22 +0800

    雛形：購物車

--- routes/web.php
+++ routes/web.php
@@ -25,18 +25,23 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 #修改會員資料頁面
 Route::get('/user/change',[\App\Http\Controllers\UserController::class,'edit'])->name('user.change');
-#更新會員資料
-Route::patch('/user/{id}',[\App\Http\Controllers\UserController::class,'update'])->name('user.update');
-
 #瀏覽點餐紀錄頁面
 Route::get('/order',[\App\Http\Controllers\OrderController::class,'index'])->name('order.history');
 #點餐詳細頁面
 Route::get('/order/item/{id}',[\App\Http\Controllers\OrderController::class,'show'])->name('order.item');
-
 #問題回報頁面
 Route::get('/user/question',[\App\Http\Controllers\CommentsController::class,'index'])->name('user.question');
+#購物車頁面
+Route::get('/user/cart',[\App\Http\Controllers\CartController::class,'index'])->name('user.cart');
+
+
+
+#更新會員資料
+Route::patch('/user/{id}',[\App\Http\Controllers\UserController::class,'update'])->name('user.update');
 #問題送出
 Route::post('/user/store',[\App\Http\Controllers\CommentsController::class,'store'])->name('user.store');
+
+
 
 #後台根路由
 Route::get('/manage',[\App\Http\Controllers\ManageController::class,'index'])->name('manage.index');
