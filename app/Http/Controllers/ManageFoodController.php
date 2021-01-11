92f463dea35bb19c01a9a7b82eaec9d83a3c577a
yu-jie shen <rock.02130529@gmail.com>
2021-01-11 14:36:57 +0800

    food controller index功能

--- app/Http/Controllers/ManageFoodController.php
+++ app/Http/Controllers/ManageFoodController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Food;
 use Illuminate\Http\Request;
 
 class ManageFoodController extends Controller
 {
     public function index()
     {
-        return view('manage.food.index');
+        $food = Food::OrderBy('id','DESC')->get();
+        $data=['food'=>$food];
+        return view('manage.food.index',$data);
     }
 
     public function create()
