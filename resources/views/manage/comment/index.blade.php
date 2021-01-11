26a59e60ade03fa0597749db7bd6680d45163e25
yu-jie shen <rock.02130529@gmail.com>
2021-01-11 14:35:47 +0800

    顧客問題回報view功能修正&測試

--- resources/views/manage/comment/index.blade.php
+++ resources/views/manage/comment/index.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layouts.master')
+@extends('manage.layouts.master')
 
 @section('title', '顧客評論')
 
@@ -7,12 +7,10 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">
-                顧客評論 <small>所有評論列表</small>
+                顧客回報<small>所有回報列表</small>
             </h1>
             <ol class="breadcrumb">
-                <li class="active">
-                    <i class="fa fa-edit"></i> 文章管理
-                </li>
+
             </ol>
         </div>
     </div>
@@ -26,23 +24,24 @@
                 <table class="table table-bordered table-hover">
                     <thead>
                     <tr>
-                        <th width="30" style="text-align: center">#</th>
-                        <th>標題</th>
-                        <th width="70" style="text-align: center">精選？</th>
-                        <th width="100" style="text-align: center">功能</th>
+                        <th width="30" style="text-align: center">問題號碼</th>
+                        <th width="70">客戶名</th>
+                        <th width="70" style="text-align: center">回報時間</th>
+                        <th  style="text-align: center">報怨內容</th>
+
+
+
                     </tr>
                     </thead>
                     <tbody>
-                    @foreach(range(1, 20) as $id)
+                    @foreach($comments as $comment)
                         <tr>
-                            <td style="text-align: center">{{ $id }}</td>
-                            <td>評論標題</td>
-                            <td style="text-align: center">V</td>
-                            <td>
-                                <a href="{{ route('admin.posts.edit', $id) }}">編輯</a>
-                                /
-                                <a href="#">刪除</a>
-                            </td>
+                            <td style="text-align: center">{{ $comment->id }}</td>
+                            <td style="text-align: center">{{ $comment->user_id }}</td>
+                            <td style="text-align: center">{{ $comment->date }}</td>
+                            <td style="text-align: center">{{ $comment->content }}</td>
+
+
                         </tr>
                     @endforeach
                     </tbody>
