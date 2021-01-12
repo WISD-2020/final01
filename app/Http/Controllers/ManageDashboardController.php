62302edbae58ad01de054ec88736ee94700f55ec
yu-jie shen <rock.02130529@gmail.com>
2021-01-12 12:05:56 +0800

    後台頁面登入驗證

--- app/Http/Controllers/ManageDashboardController.php
+++ app/Http/Controllers/ManageDashboardController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ManageDashboardController extends Controller
 {
+
     public function index()
     {
-        return view('manage.dashboard.index');
+        $name=Auth::user();
+
+
+        if (Auth::check()&& auth()->user()->class==1) {
+            // 已登入
+
+                return view('manage.dashboard.index', $name);
+        }
+        else
+        {
+            return redirect()->route('login');
+        }
+
     }
 }
