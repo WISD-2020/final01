4bcb9b9245c7c3437a1e05d874b97bb744b71f21
yu-jie shen <rock.02130529@gmail.com>
2021-01-12 21:19:23 +0800

    訂單controller功能

--- app/Http/Controllers/ManageOrderController.php
+++ app/Http/Controllers/ManageOrderController.php
@@ -2,19 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class ManageOrderController extends Controller
 {
     public function index()
     {
-        return view('manage.order.index');
+        $order =Order::OrderBy('id','DESC')->get();
+        $data=['orders'=>$order];
+        return view('manage.order.index',$data);
     }
 
     public function edit($id)
     {
-        $data=['id'=>$id];
+        $order=Order::find($id);
+        $data=['food'=>$order];
 
-        return view('manage.order.index',$data);
+        return view('manage.order.edit',$data);
+    }
+
+    public function update(Request $request,$id)
+    {
+        $order=Order::find($id);
+        $order->update($request->all());
+        return redirect()->route('manage.order.index');
+    }
+
+
+    public function destroy($id)
+    {
+        Order::destroy($id);
+        return redirect()->route('manage.order.index');
     }
 }
