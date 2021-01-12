c848cf84307d56e4487916e5cad75387ed75d06a
Lin Yu Lun <wuyahayaa01@gmail.com>
2021-01-12 18:58:26 +0800

    修正：餐點編號

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,7 +31,7 @@
                     <input type="hidden" name="user_id" value="{{$name->name}}">
                 <table class="table table-bordered table-hover">
                     <thead>
-                    @foreach($foods as $food)
+
                         <tr>
                             <th width="15%" style="text-align: center">餐點編號</th>
                             <th width="20%" style="text-align: center">圖片</th>
@@ -43,14 +43,13 @@
                     </thead>
                     <tbody>
 
-
+                    @foreach($foods as $food)
                     <tr>
                         <td style="text-align: center;line-height:100px;">
                             {{$food->id}}
-                            <input type="hidden" name="food_id" value="{{$food->id}}">
                         </td>
                         <td style="text-align: center;line-height:100px;">
-                            {{$food->image}}<img src="img/french-fries.jpg" width="100" height="100">
+                           <img src="{{$food->image}}" width="100" height="100">
                         </td>
                         <td style="text-align: center;line-height:100px;">
                             {{$food->name}}
@@ -59,12 +58,10 @@
                             {{$food->price}}
                         </td>
                         <td style="text-align: center;vertical-align: middle">
-                            <form action="" method="post">
-                            <input type="text" name="amount" class="form-control" value="1">
-                            </form>
+                            <input type="number" name="amount" min="1" max="99" value="1">
                         </td>
                         <td style="text-align: center;line-height:100px;">
-                            <button type="submit" class="btn btn-sm btn-primary">送出</button>
+                            <button type="submit" class="btn btn-sm btn-primary" name="food_id" value="{{$food->id}}">送出</button>
                         </td>
                     </tr>
 
