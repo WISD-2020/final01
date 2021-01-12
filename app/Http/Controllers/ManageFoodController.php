8fb326142cade67b4318b2790b13dcd84c507f3a
yu-jie shen <rock.02130529@gmail.com>
2021-01-12 21:17:39 +0800

    菜單圖片功能測試

--- app/Http/Controllers/ManageFoodController.php
+++ app/Http/Controllers/ManageFoodController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Food;
 use Illuminate\Http\Request;
+use function Illuminate\Events\queueable;
 
 class ManageFoodController extends Controller
 {
@@ -21,16 +22,40 @@ class ManageFoodController extends Controller
 
     public function edit($id)
     {
-        $data=['id'=>$id];
+        $food=Food::find($id);
+        $data=['food'=>$food];
 
         return view('manage.food.edit',$data);
     }
 
     public function store(Request $request)
     {
-        food::create($request->all());
+        $food=new Food();
+
+
+        $food->id = $request->id;
+
+       $image=$request->file('image');
+       $imgname=time().'.'.$image->getClientOriginalExtension();
+        $request->image->move(public_path('/img'),$imgname);
+        $food->image="img/".$imgname;
+
+        $food->name = $request->name;
+        $food->price = $request->price;
+        $food->is_selling = $request->is_selling;
+        $food->is_hot = $request->is_hot;
+
+        $food->save();
+
         return redirect()->route('manage.food.index');
 
+    }
+
+    public function update(Request $request,$id)
+    {
+        $food=Food::find($id);
+        $food->update($request->all());
+        return redirect()->route('manage.food.index');
     }
 
     public function destroy($id)
